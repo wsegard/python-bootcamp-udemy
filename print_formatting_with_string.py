8c6205a684011839753e26db499b32731489b77c
william segard <william.segard@gmail.com>
2019-02-19 21:19:30 +0100

    print formatting example

---
+++ print_formatting_with_string.py
@@ -0,0 +1,11 @@
+print('This is a string {}'.format('INSERTED'))
+print ('the {} {} {}'.format('fox', 'brown', 'quick'))
+print ('the {1} {2} {0}'.format('fox', 'brown', 'quick'))
+print ('the {f} {b} {q}'.format(f='fox', b='brown', q='quick'))
+result = 100/177
+print(result)
+print('The result was {}'.format(result))
+#float formatting follows "{values:width.prcision f}"
+print('The result was {r:1.3f}'.format(r=result))
+name = 'Jose'
+#print(f 'Hello, his name is {name}')
